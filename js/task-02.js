bcb1b1392cd3e84fedb93de47e168ce28c5fb89b
Artem Grokha <grokha.artem@gmail.com>
2022-10-19 22:31:03 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+console.log(ingredients);
+
+const newLiEl = document.createElement("li");
+newLiEl.textContent = "Potatoes";
+newLiEl.classList.add("item");
+//console.log(newLiEl);
+
+const ingredientsEl = document.querySelector("#ingredients");
+ingredientsEl.appendChild(newLiEl);
+console.log(ingredientsEl);
