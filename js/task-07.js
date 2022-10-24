0708b72e92e84461ca7adc6b9d86ef9fa9a52ff6
Artem Grokha <grokha.artem@gmail.com>
2022-10-24 19:49:40 +0300

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const sizeEl = document.querySelector("#font-size-control");
+const textEl = document.querySelector("#text");
+
+textEl.style.fontSize = sizeEl.value + "px";
+
+sizeEl.addEventListener("input", (event) => {
+  console.log(event.target.value);
+  textEl.style.fontSize = `${event.target.value}px`;
+});
