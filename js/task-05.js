b5d161e4e82cf61a0450519f7c8be1617231c6a6
Artem Grokha <grokha.artem@gmail.com>
2022-10-24 18:40:39 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,3 +2,13 @@
 
 const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
+
+const inputTextEl = () => {
+  if (inputEl.value.length > 0) {
+    outputEl.textContent = inputEl.value;
+  } else {
+    outputEl.textContent = "Anonymous";
+  }
+};
+
+inputEl.addEventListener("input", inputTextEl);
