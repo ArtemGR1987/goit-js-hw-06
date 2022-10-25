b02c5a1eecbd29c77631f7ee5f6312a7c590e9ef
Artem Grokha <grokha.artem@gmail.com>
2022-10-25 16:08:51 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+"use strict";
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onSubmit);
+function onSubmit(event) {
+  event.preventDefault();
+  const email = event.target.elements.email.value;
+  const pass = event.target.elements.password.value;
+
+  if (!email || !pass) {
+    return alert("Заполните, пожалуйста, все поля");
+  }
+
+  const Obj = { email, pass };
+  console.log(Obj);
+  form.reset();
+}
